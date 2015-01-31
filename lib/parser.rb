a32005181d2abd52bcd9fc46ab706477d9492af8
Karen Bowen <the.miinx@gmail.com>
2015-02-01 00:39:11 +1100

    Add parsing of camera make from works xml

--- lib/parser.rb
+++ lib/parser.rb
@@ -14,6 +14,7 @@ class Parser < Nokogiri::XML::SAX::Document
   end
 
   def start_element(name, attrs=[])
+    @current_attrs = attrs
     if name == "work"
       @current_work = Work.new
     end
@@ -22,7 +23,19 @@ class Parser < Nokogiri::XML::SAX::Document
   def end_element(name)
     if name == "work"
       @works.push(@current_work)
+    else
+      if self.respond_to?("on_#{name}")
+        self.method("on_#{name}").call(@current_characters, @current_attrs)
+      end
     end
+  end
+
+  def characters(characters)
+    @current_characters = characters
+  end
+
+  def on_make(make, attrs)
+    @current_work.make = make
   end
 
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,16 +4,21 @@ describe "Parser" do
 
   before(:context) do
     @xml = "<works>" \
-            "<work></work>" \
-            "<work></work>" \
+            "<work><exif><make>Canon</make></exif></work>" \
+            "<work><exif><make>Nikon</make></exif></work>" \
            "</works>"
 
     @parser = Parser.new
+    @parser.parse_input(@xml)
   end
 
   it "parses the input xml" do
-    @parser.parse_input(@xml)
     expect(@parser.works.size).to eq(2)
+    expect(@parser.works[0]).to be_instance_of(Work)
+  end
+
+  it "parses the camera makes" do
+    expect(@parser.works[0].make).to eq("Canon")
   end
 
 end
